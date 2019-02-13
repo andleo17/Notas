d39c2836a16b524f882b23df0f3aa9e2bb7ac043
andleo <andres.gastulo@gmail.com>
2019-02-13 11:33:16 -0500

    Creando la clase Main

---
+++ src/com/andres/notas/Main.java
@@ -0,0 +1,15 @@
+package com.andres.notas;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andrés
+ */
+public class Main {
+    
+}
